80850d81fecf2edee86a8c665a5436ca54be0e30
Kolawole Abdulrahman <backendkolawole@gmail.com>
2024-05-06 04:05:37 +0100

    Time: 99 ms (21.19%) | Memory: 16.7 MB (7.31%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,29 +1,32 @@
-# declare a set variable that will store values in each row, column and box
-# iterate through each cell in the matrix row by row
-# for each cell in the matrix
-# if the current cell is present in the row, or present in the column or present in the box return false
-# return true 
+# iterate through the board row by row
+# for each cell in the board,
+# if the current cell is present in the the set of rows return false, else add it to the set of rows
+# repeat the same process for columns and subboxes
+# return true at the end of the iteration
 
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = len(board)
+        columns = len(board[0])
         my_set = set()
 
-        for i in range(9):
-            for j in range(9):
+        for i in range(rows):
+            for j in range(columns):
                 cell = board[i][j]
-                if (cell == "."):
-                    continue
-                # box_index = (3 * (i // 3)) + j // 3
-                box_index = 3 * math.floor(i / 3) + math.floor(j / 3)
+                box_index = (3 *  math.floor(i / 3)) + math.floor(j / 3)
 
-                row = f"row: ${i}, cell: ${cell}"
-                column = f"column: ${j}, cell: ${cell}"
-                box = f"box: ${box_index}, cell: ${cell}"
-                if (row in my_set or column in my_set or box in my_set):
-                    return False
-                
-                my_set.add(row)
-                my_set.add(column)
-                my_set.add(box)
+                if (cell != '.'):
+                    row = f"row: {i}, cell: {cell}"
+                    column = f"column: {j}, cell: {cell}"
+                    sub_box = f"sub_box: {box_index}, cell: {cell}"
+                    print(row)
+
+                    if (row in my_set or column in my_set or sub_box in my_set):
+                        return False
+                    my_set.add(row)
+                    my_set.add(column)
+                    my_set.add(sub_box)
         
         return True
+
+        
