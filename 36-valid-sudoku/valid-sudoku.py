ce70cb42fdaa05bb5530a17e91027201f4a58d96
Kolawole Abdulrahman <backendkolawole@gmail.com>
2024-06-23 09:30:22 +0100

    Time: 89 ms (72.28%) | Memory: 16.6 MB (46.40%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,14 +1,15 @@
-# iterate through the board row by row
-# for each cell in the board,
-# if the current cell is present in the the set of rows return false, else add it to the set of rows
-# repeat the same process for columns and subboxes
-# return true at the end of the iteration
+# the sudoku board is valid if each row, column and subbox contain unique elements
+# declare three set variables rows. columns and subbx
+# iterate through the board
+# if the current cell is already present in a row, column or subbox return false
+# else put it in 
 
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
+        my_set = set()
         rows = len(board)
         columns = len(board[0])
-        my_set = set()
+
 
         for i in range(rows):
             for j in range(columns):
@@ -19,14 +20,13 @@ class Solution:
                     row = f"row: {i}, cell: {cell}"
                     column = f"column: {j}, cell: {cell}"
                     sub_box = f"sub_box: {box_index}, cell: {cell}"
-                    print(row)
-
                     if (row in my_set or column in my_set or sub_box in my_set):
                         return False
                     my_set.add(row)
                     my_set.add(column)
                     my_set.add(sub_box)
-        
+            
         return True
+                
 
         
