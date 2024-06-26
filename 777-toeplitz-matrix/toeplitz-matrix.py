37777e1574454d3fd69e851bad698805a6b5faf1
Kolawole Abdulrahman <backendkolawole@gmail.com>
2024-06-26 12:08:56 +0100

    Time: 83 ms (21.73%) | Memory: 16.4 MB (90.36%) - LeetSync

---
+++ 777-toeplitz-matrix/toeplitz-matrix.py
@@ -0,0 +1,35 @@
+# define a function isDiagonalUniValue that returns true or false if every cell in the diagonal is the same
+# the function takes two dimensions i and j and a prevValue
+# while the dimensions are still in bounds 
+# if the current element is not equal to the prevValue return false
+# else, increment row and column by 1
+# iterate through the columns of the grid and call isDiagonaluNI on the cells
+# iterate through the grid and call isDiang on the cells
+# 
+
+class Solution:
+    def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
+        rows = len(matrix)
+        columns = len(matrix[0])
+
+        def isDiagonalUniValue(i, j, prevValue):
+            while (i < rows and j < columns):
+                if (matrix[i][j] != prevValue):
+                    return False
+                i += 1
+                j += 1
+            
+            return True
+
+        for i in range(columns):
+            if (not isDiagonalUniValue(0, i, matrix[0][i])):
+                return False
+        
+        for i in range(1, rows):
+            if (not isDiagonalUniValue(i, 0, matrix[i][0])):
+                return False
+
+        return True
+            
+
+        
