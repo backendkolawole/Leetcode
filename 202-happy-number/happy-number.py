a612196987333c8fc8ee406b793d7a24f3558ab9
Kolawole Abdulrahman <backendkolawole@gmail.com>
2024-08-01 13:31:59 +0100

    Time: 33 ms (80.54%) | Memory: 16.4 MB (60.82%) - LeetSync

---
+++ 202-happy-number/happy-number.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        n = str(n)
+        my_set = set()
+
+        while n:
+            n = str(n)
+            sum = 0
+
+            for char in n:
+                sum += int(char)**2
+            
+            if (sum == 1):
+                return True
+            if (sum in my_set):
+                return False
+            my_set.add(sum)
+
+            n = sum
+        
+        return False
+        
