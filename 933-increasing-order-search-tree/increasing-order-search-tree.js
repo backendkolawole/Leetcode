8a5bcf9d7a97fb12848f47a76cf31aa656ea182c
Kolawole Abdulrahman <backendkolawole@gmail.com>
2024-08-09 10:30:20 +0100

    Time: 60 ms (28.23%) | Memory: 50.9 MB (20.00%) - LeetSync

--- 933-increasing-order-search-tree/increasing-order-search-tree.js
+++ 933-increasing-order-search-tree/increasing-order-search-tree.js
@@ -7,40 +7,22 @@
  * }
  */
 
-// var increasingBST = function(root) {
-//     let inorder = []
-//     if (!root) return inorder
-
-//     function traverse(node) {
-//         if (!node) return 
-//         traverse(node.left)
-//         inorder.push(node)
-//         traverse(node.right)
-//     }
-//     traverse(root)
-//     console.log(inorder)
-
-//     for (let i = 1; i < inorder.length; i++) {
-//         inorder[i - 1].right = inorder[i]
-//         inorder[i - 1].left = null
-//     }
-//     return inorder[0]
-// };
-
 var increasingBST = function(root) {
-    let inorder = [];
-    if (!root) return null;
+    let inorder = []
+    if (!root) return inorder
 
     function traverse(node) {
-        if (!node) return;
-        traverse(node.left);
-        inorder.push(new TreeNode(node.val));
-        traverse(node.right);
+        if (!node) return 
+        if (node.left) traverse(node.left)
+        inorder.push(new TreeNode(node.val))
+        if (node.right) traverse(node.right)
     }
-    traverse(root);
+    traverse(root)
 
     for (let i = 1; i < inorder.length; i++) {
-        inorder[i - 1].right = inorder[i];
+        inorder[i - 1].right = inorder[i]
+        inorder[i - 1].left = null
     }
-    return inorder[0];
+    return inorder[0]
 };
+
