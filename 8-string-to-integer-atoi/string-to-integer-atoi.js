3d9f52fc9b3dbbe8d1dd42b64b54302adb937dbb
Kolawole Abdulrahman <backendkolawole@gmail.com>
2024-08-11 11:31:26 +0100

    Time: 111 ms (6.65%) | Memory: 55.1 MB (5.47%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.js
@@ -0,0 +1,49 @@
+var myAtoi = function(s) {
+    s = s.trim()
+    if (!s) return 0
+    console.log(isNaN(' '))
+
+    i = 0
+    let sign = 1
+
+    let char = s[i]
+    if (char == '+') {
+        i += 1
+    }
+    
+    else if (char == '-') {
+        sign = -1
+        i += 1
+    }
+
+    let result = ""
+    while (i < s.length) {
+        let cur = s[i]
+
+        if (isNaN(cur) || cur == ' ') {
+            console.log(cur)
+            break
+        }
+
+        else result += cur 
+        
+        i ++
+    
+    }
+    console.log('here')
+    // if (!result) return 0
+
+    // if (result >= Math.pow(2, 32) - 1) return (Math.pow(2, 31) - 1)
+    // else if (result <= - Math.pow(2, 31)) return Math.pow(-2, 31)
+    // else return result
+
+    if (result) {
+        result = parseInt(result) * sign
+        if (result <= -2147483648) return -2147483648;
+        else if (result >= 2147483647) return 2147483647;
+        else return result;
+    }
+    return 0;
+
+
+};
