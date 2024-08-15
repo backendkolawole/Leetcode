42cc63e956ed0f59945e3b7ced8d77376948bf8f
Kolawole Abdulrahman <backendkolawole@gmail.com>
2024-08-15 10:01:50 +0100

    Time: 1449 ms (15.71%) | Memory: 22.1 MB (5.31%) - LeetSync

---
+++ 15-3sum/3sum.py
@@ -0,0 +1,30 @@
+# take the current element 
+# run twosum on the rest of the array
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        
+        output = []
+        lookup = set()
+        nums.sort()
+
+        for i in range(len(nums)):
+            first = nums[i]
+            a = i + 1
+            b = len(nums) - 1
+
+            while (a < b):
+                sum = first + nums[a] + nums[b]
+                if (sum == 0):
+                    value = (first, nums[a], nums[b])
+                    if value not in lookup:
+                        lookup.add(value)
+                        output.append([first, nums[a], nums[b]])
+                    a += 1
+                    b -= 1
+                elif (sum < 0):
+                    a += 1
+                else:
+                    b -= 1
+
+        return output
