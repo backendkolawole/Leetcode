69c9bca370a8686e911774d0f80aba1804ac6050
Kolawole Abdulrahman <backendkolawole@gmail.com>
2024-08-19 12:32:21 +0100

    Time: 41 ms (13.51%) | Memory: 16.5 MB (69.99%) - LeetSync

---
+++ 556-next-greater-element-iii/next-greater-element-iii.py
@@ -0,0 +1,31 @@
+class Solution:
+    def nextGreaterElement(self, n: int) -> int:
+        array = [char for char in str(n)]
+        index = -1
+        length = len(array)
+        index = len(array) - 2
+
+        while (index >= 0 and array[index] >= array[index + 1]):
+            index -=1
+
+        if (index == -1):
+            return -1
+
+        swap = length - 1
+
+        while (array[swap] <= array[index]):
+            swap -=1
+
+        array[index], array[swap] = array[swap], array[index]
+
+        i = index + 1
+        j = length - 1
+
+        while i < j:
+            array[i], array[j] = array[j], array[i]
+            i += 1
+            j -= 1
+
+        result = int(''.join(list(array)))
+        return -1 if pow(2, 31) - 1 < result else result
+        
