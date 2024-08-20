585f85ba2419f3986e9f485147dfd96c32683abd
Kolawole Abdulrahman <backendkolawole@gmail.com>
2024-08-20 16:07:45 +0100

    Time: 190 ms (12.02%) | Memory: 64.9 MB (5.29%) - LeetSync

---
+++ 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.js
@@ -0,0 +1,28 @@
+
+var findPairs = function(nums, k) {
+    let lookup = {}
+    
+    for (let num of nums) {
+        if (!lookup[num]) lookup[num] = 0
+        lookup[num] += 1
+    }
+
+    let count = 0
+    
+    for ([key, val] of Object.entries(lookup)) {
+            console.log(typeof key, typeof val)
+        }
+
+    if (k == 0) {
+        for ([key, val] of Object.entries(lookup)) {
+            if (val > 1) count +=1
+        }
+        
+    }
+    else {
+        for ([key, val] of Object.entries(lookup)) {
+            if (lookup[Number(key) + k]) count ++
+        }
+    }
+    return count
+};
