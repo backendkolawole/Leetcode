d26b027fc3505a48ee34ce5deaf766422f648fd6
Kolawole Abdulrahman <backendkolawole@gmail.com>
2024-08-24 10:57:03 +0100

    Time: 305 ms (18.30%) | Memory: 19.1 MB (11.44%) - LeetSync

---
+++ 524-longest-word-in-dictionary-through-deleting/longest-word-in-dictionary-through-deleting.py
@@ -0,0 +1,38 @@
+# sort the input string
+# iterate through dictionary
+
+# check if the current string can be formed by deleting characters from string s
+# if true
+# if the size of the current string is greater than the size of the result string, 
+# update the result string
+# if the size of the current string is equal to the result string, update the result based on lexicographical order
+
+
+class Solution:
+    def findLongestWord(self, s: str, dictionary: List[str]) -> str:
+        lookup = {x: i for i, x in enumerate(s)}
+        result = ""
+
+        def canForm(char, target):
+            i = 0
+            j = 0
+
+            while i < len(char) and j < len(target):
+                if (char[i] == target[j]):
+                    i += 1
+                    j += 1
+                else:
+                    i+=1
+            
+            return j == len(target)
+
+
+        for string in dictionary:
+            if(canForm(s, string)):
+                size = len(string)
+                if (size > len(result)):
+                    result = string
+                elif (size == len(result)):
+                    result = result if result < string else string
+
+        return result
