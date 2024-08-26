8d11dc552fdc048451483d06263aca53277f3f22
Kolawole Abdulrahman <backendkolawole@gmail.com>
2024-08-26 12:21:14 +0100

    Time: 238 ms (28.23%) | Memory: 21.5 MB (60.43%) - LeetSync

---
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.py
@@ -0,0 +1,18 @@
+class Solution:
+    def subarraysDivByK(self, nums: List[int], k: int) -> int:
+        lookup = {0: 1}
+        result = 0
+        total = 0
+
+        for i, val in enumerate(nums):
+            total += val
+            remainder = (total % k + k) % k
+
+            if (remainder not in lookup):
+                lookup[remainder] = 0
+            else:
+                result += lookup[remainder]
+            lookup[remainder] +=1
+
+        
+        return result
