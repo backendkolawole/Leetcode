613fde8ed0a96372829923864d5f72d2982f8571
Kolawole Abdulrahman <backendkolawole@gmail.com>
2024-10-18 09:06:08 +0100

    Time: 72 ms (51.50%) | Memory: 57.9 MB (97.04%) - LeetSync

---
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.js
@@ -0,0 +1,15 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+
+var lowestCommonAncestor = function(root, p, q) {
+    if (root.val == p.val || root.val == q.val) return root
+    if (p.val < root.val && q.val < root.val) return lowestCommonAncestor(root.left, p, q)
+    if (p.val > root.val && q.val > root.val) return lowestCommonAncestor(root.right, p, q)
+    return root
+};
