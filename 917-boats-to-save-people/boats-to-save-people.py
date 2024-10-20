43ba81028cfae5ff25a13ce333edad154f592264
Kolawole Abdulrahman <backendkolawole@gmail.com>
2024-10-20 10:12:24 +0100

    Time: 46 ms (99.80%) | Memory: 22.5 MB (97.99%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.py
+++ 917-boats-to-save-people/boats-to-save-people.py
@@ -1,16 +1,22 @@
+# sort the input array
+# declare two pointers i and j with initial value 0 and len(people) - 1
+# while the two pointers havent met
+# pick up the heaviest person on the boat
+# if there is room for another person, pick up that person and move the i pointer forward
+
 class Solution:
     def numRescueBoats(self, people: List[int], limit: int) -> int:
         people.sort()
+
         i = 0
         j = len(people) - 1
-        result = 0
+        count = 0
 
         while (i <= j):
             remain = limit - people[j]
-            result += 1
-            j -= 1
+            if (people[i] <= remain):
+                i += 1
+            count += 1
+            j -=1
 
-            if (i <= j) and people[i] <= remain:
-              i += 1
-
-        return result  
+        return count
