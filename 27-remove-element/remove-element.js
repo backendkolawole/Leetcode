a6355d49e0461feb114711d7d05c1cd94780cf86
Kolawole Abdulrahman <backendkolawole@gmail.com>
2024-12-14 13:02:21 +0100

    Time: 0 ms (100.00%) | Memory: 49.8 MB (7.64%) - LeetSync

---
+++ 27-remove-element/remove-element.js
@@ -0,0 +1,13 @@
+var removeElement = function(nums, val) {
+    k = 0
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] != val) {
+            nums[k] = nums[i]
+            k += 1
+        }
+    }
+
+    return k
+
+};
