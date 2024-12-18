f1a755fffa61f782fe149fefaed73adf22575c20
Kolawole Abdulrahman <backendkolawole@gmail.com>
2024-12-18 10:26:50 +0100

    Time: 7 ms (75.04%) | Memory: 55.9 MB (99.83%) - LeetSync

--- 463-island-perimeter/island-perimeter.js
+++ 463-island-perimeter/island-perimeter.js
@@ -1,24 +1,25 @@
-// iterate the grid row by row
-// if we are on land
-// if theres a neighbor to the left, reduce the perimeter by 2
-// if theres a neigbor at the top, reduce the perimeter by 2
+// iterate through the grid
+// if the current cell is land, increase the perimeter by 4
+// if the current cell has a left neighbor, decrease the perimeter by 2
+// if the current cell has a right neightbor, decrease perimeter by 2
+// return perimeter
 
 var islandPerimeter = function(grid) {
-    let perimeter = 0
     let rows = grid.length
     let columns = grid[0].length
+    let perimeter = 0
 
     for (let i = 0; i < rows; i++) {
         for (let j = 0; j < columns; j++) {
             cell = grid[i][j]
-
             if (cell == 1) {
                 perimeter += 4
-                if (j > 0 && grid[i][j - 1] == 1) perimeter -= 2
-                if (i > 0 && grid[i - 1][j] == 1) perimeter -= 2
+                if (j - 1 >= 0 && grid[i][j - 1] == 1) perimeter -= 2
+                if (i - 1 >= 0 && grid[i - 1][j] == 1) perimeter -= 2
             }
         }
     }
 
-    return perimeter    
+    return perimeter
+    
 };
